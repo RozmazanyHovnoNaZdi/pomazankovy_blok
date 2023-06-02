23da583f77cb64d28ccc9aad394c0f8ff892798a
jakub.matejka <jakub.matejka@VT1-06>
2023-06-02 09:10:21 +0200

    Přidejte soubory projektu.

---
+++ pomazankovy blok/MainWindow.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Windows;
+using Microsoft.Win32;
+
+namespace poznámkovy_blok
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+
+        private void MenuItem_Otevri_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog otvorSoubor = new OpenFileDialog();
+
+            if (otvorSoubor.ShowDialog() == true)
+            {
+                string danejsoubor = otvorSoubor.FileName;
+
+                ObsahSouboru.Text = File.ReadAllText(danejsoubor);
+
+            }
+        }
+
+
+
+        private void MenuItem_Uloz_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog ulozSoubor = new SaveFileDialog();
+
+            if (ulozSoubor.ShowDialog() == true)
+            {
+                string danejsoubor = ulozSoubor.FileName;
+
+                File.WriteAllText(danejsoubor, ObsahSouboru.Text);
+            }
+        }
+
+        private void Buttonmax(object sender, RoutedEventArgs e)
+        {
+            ObsahSouboru.FontSize += 2;
+        }
+
+        private void Buttonmin(object sender, RoutedEventArgs e)
+        {
+            ObsahSouboru.FontSize -= 2;
+        }
+
+        private void Buttontrash(object sender, RoutedEventArgs e)
+        {
+            ObsahSouboru.Clear();
+        }
+
+        private void Buttonamount(object sender, RoutedEventArgs e)
+        {
+            int pocetZnaku = ObsahSouboru.Text.Length;
+            MessageBox.Show("znaky: " + pocetZnaku.ToString());
+        }
+        private void ObsahSouboru_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+         
+        }
+    }
+}
